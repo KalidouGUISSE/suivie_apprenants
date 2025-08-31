41d87e87e030abbf84c032364318fa5a0146543a
Papa Sidy Fall <papasidyfall03@gmail.com>
2025-08-31 15:41:22 +0000

    modification au niveau de la route niveau

--- src/routes/niveau.routes.ts
+++ src/routes/niveau.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
 import { NiveauController } from "../controllers/niveau.controller.js";
 import { createNiveauSchema } from "../validators/niveau.validator.js";
-import { validateBody } from "../utils/middlewares/validateBody.js";
 
 const router = Router();
 
-router.post("/", validateBody(createNiveauSchema), NiveauController.create);
+router.post("/",NiveauController.create);
 router.get("/", NiveauController.getAll);
-router.get("/:id", NiveauController.getOne);
-router.put("/:id", validateBody(createNiveauSchema), NiveauController.update);
+router.get("/:id",NiveauController.getOne);
+router.put("/:id",NiveauController.update);
 router.delete("/:id", NiveauController.delete);
 
 export default router;
